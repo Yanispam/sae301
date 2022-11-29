fc1425c0825d3653bad2c9f4e93c2255c897dd5e
Yanis Pamadou <100587894+Yanispam@users.noreply.github.com>
2022-11-29 08:45:47 +0100

    update

--- src/Controller/TheatreController.php
+++ src/Controller/TheatreController.php
@@ -2,6 +2,8 @@
 
 namespace App\Controller;
 
+use App\Repository\LieuxRepository;
+use App\Repository\ManifsRepository;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
@@ -9,10 +11,12 @@ use Symfony\Component\Routing\Annotation\Route;
 class TheatreController extends AbstractController
 {
     #[Route('/theatre', name: 'app_theatre')]
-    public function index(): Response
+    public function afficherManif(ManifsRepository $ManifsRepository): Response
     {
+
+        $Manifs = $ManifsRepository->findAll();
         return $this->render('theatre/index.html.twig', [
-            'controller_name' => 'TheatreController',
+            'Manifs' => $Manifs,
         ]);
     }
 }
