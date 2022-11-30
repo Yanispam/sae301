e81501a9c7356a4fd291a3898f431279b8980322
Yanis Pamadou <100587894+Yanispam@users.noreply.github.com>
2022-11-30 10:20:25 +0100

    update

--- src/Controller/ManifsController.php
+++ src/Controller/ManifsController.php
@@ -2,6 +2,8 @@
 
 namespace App\Controller;
 
+use App\Repository\LieuxRepository;
+use App\Repository\ManifsRepository;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
@@ -9,10 +11,20 @@ use Symfony\Component\Routing\Annotation\Route;
 class ManifsController extends AbstractController
 {
     #[Route('/manifs', name: 'app_manifs')]
-    public function index(): Response
+    public function afficherManif(ManifsRepository $ManifsRepository): Response
     {
+
+        $Manifs = $ManifsRepository->findAll();
         return $this->render('manifs/index.html.twig', [
-            'controller_name' => 'ManifsController',
+            'Manifs' => $Manifs,
+        ]);
+    }
+    #[Route('/manifs/{id}', name: 'app_details')]
+    public function afficherDetail(ManifsRepository $ManifsRepository, $id): Response
+    {
+        $Manifs = $ManifsRepository->find($id);
+        return $this->render('manifs/manifsdetails.html.twig', [
+            'Manifs' => $Manifs,
         ]);
     }
 }
