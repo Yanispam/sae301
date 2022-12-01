d77db5c6b5693da93541b322b344c32178a3dd7b
Yanis Pamadou <100587894+Yanispam@users.noreply.github.com>
2022-12-01 09:55:27 +0100

    update

--- public/scriptpagearticles.js
+++ public/scriptpagearticles.js
@@ -1,18 +1,19 @@
 document.getElementById('ajout').addEventListener('click',function() {                                                              
     var id = document.getElementById('id').value
-    var article= document.getElementById('article').innerHTML
+    var titre= document.getElementById('titre').innerHTML
     var prix= document.getElementById('prix').innerHTML
-    console.log( id + " " + article + " " + prix )
+    console.log( id + " " + titre + " " + prix )
+    console.log('click')
 
     index = montab.findIndex(element => element.id == id); //trouver l'article dans la liste du panier
     if(index>-1){		
-                // console.log("l'article est deja dans le panier, il faut juste incrementer la qte")
+                console.log("l'article est deja dans le panier, il faut juste incrementer la qte")
                 montab[index].quantite	= parseInt(montab[index].quantite) +parseInt(document.getElementById('qte').value)
                 console.log(montab)
                 }
     else        {
-                // console.log("l'article n'est pas pour l'instant dans le panier, il va falloir l'ajouter")
-                montab.push({ 'id': id, 'article': article, 'quantite': document.getElementById('qte').value , 'prix': prix})
+                console.log("l'article n'est pas pour l'instant dans le panier, il va falloir l'ajouter")
+                montab.push({ 'id': id, 'titre': titre, 'quantite': document.getElementById('qte').value , 'prix': prix})
                 console.log(montab)
                 }
     
