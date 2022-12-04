26e95e7c2f6333757a1e85c0a4fbd47e82760bd9
Yanis Pamadou <100587894+Yanispam@users.noreply.github.com>
2022-12-04 14:17:30 +0100

    update

---
+++ src/Controller/FinalisationController.php
@@ -0,0 +1,19 @@
+<?php
+
+namespace App\Controller;
+
+use App\Entity\User;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+
+class FinalisationController extends AbstractController
+{
+    #[Route('/finalisation', name: 'app_finalisation')]
+    public function index(): Response
+    {
+        return $this->render('finalisation/index.html.twig', [
+            'controller_name' => 'FinalisationController',
+        ]);
+    }
+}
